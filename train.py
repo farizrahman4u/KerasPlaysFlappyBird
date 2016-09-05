1f99a5081377b2f7d082fea7545b9be7c4f0db9d
farizrahman4u <farizrahman4u@gmail.com>
2016-09-06 03:30:16 +0530

    update train args

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from flappy_bird import FlappyBird
 
 game = FlappyBird(frame_rate=10000, sounds=False)
 agent = Agent(model, memory_size=-1)
-agent.train(game, epsilon=0, epsilon_rate=0.4, gamma=0.99, nb_epoch=1000000, batch_size=32, observe=0, checkpoint=250)
+agent.train(game, epsilon=[0.1, 0.01], epsilon_rate=0.05, gamma=0.99, nb_epoch=10000, batch_size=32, observe=0, checkpoint=250)
