24b2ecace534e48adcb9fd28c8e7e1138922ca22
farizrahman4u <farizrahman4u@gmail.com>
2016-09-06 03:54:39 +0530

    change bcg and nb_frames

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 model = Sequential()
-model.add(Conv2D(32, 8, 8, subsample=(4, 4), input_shape=(1, 80, 80)))
+model.add(Conv2D(32, 8, 8, subsample=(4, 4), input_shape=(4, 80, 80)))
 model.add(MaxPooling2D())
 model.add(Activation('relu'))
 model.add(Conv2D(64, 4, 4, subsample=(2, 2)))
