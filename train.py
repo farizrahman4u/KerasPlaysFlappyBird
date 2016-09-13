86b015e2a37a03af5676e31e704213f3e2f3bd78
farizrahman4u <farizrahman4u@gmail.com>
2016-09-14 00:41:52 +0530

    new train params

--- train.py
+++ train.py
@@ -3,5 +3,5 @@ from qlearning4k import Agent
 from flappy_bird import FlappyBird
 
 game = FlappyBird(frame_rate=10000, sounds=False)
-agent = Agent(model, memory_size=-1)
-agent.train(game, epsilon=[0.1, 0.01], epsilon_rate=0.05, gamma=0.99, nb_epoch=10000, batch_size=32, observe=0, checkpoint=250)
+agent = Agent(model, memory_size=100000)
+agent.train(game, epsilon=[0.1, 0.0001], epsilon_rate=0.3, gamma=0.99, nb_epoch=1000000, batch_size=32, observe=3200, checkpoint=250)
